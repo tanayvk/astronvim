bc278ae222c4a5aae9b0aae8d9f03767cb91b8a8
Tanay Karnik <tanayvk@gmail.com>
2024-11-07 09:30:31 +0530

    fix mappings

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -1,10 +1,8 @@
-local Terminal = require('toggleterm.terminal').Terminal
+local Terminal = require("toggleterm.terminal").Terminal
 local terminals = {}
 
 local function toggle_term(num)
-  if not terminals[num] then
-    terminals[num] = Terminal:new({ direction = 'float', hidden = true })
-  end
+  if not terminals[num] then terminals[num] = Terminal:new { direction = "float", hidden = true } end
   terminals[num]:toggle()
 end
 
@@ -13,25 +11,26 @@ return {
     "AstroNvim/astrocore",
     ---@type AstroCoreOpts
     opts = {
+      mappings = {
         n = {
           ["<Leader>t1f"] = {
             function() toggle_term(1) end,
-            desc = "1st floating toggleterm"
+            desc = "1st floating toggleterm",
           },
           ["<Leader>t2f"] = {
             function() toggle_term(2) end,
-            desc = "2nd floating toggleterm"
+            desc = "2nd floating toggleterm",
           },
           ["<Leader>t3f"] = {
             function() toggle_term(3) end,
-            desc = "3rd floating toggleterm"
+            desc = "3rd floating toggleterm",
           },
           ["<Leader>t4f"] = {
             function() toggle_term(4) end,
-            desc = "4th floating toggleterm"
-          }
-        }
-      }
-    }
-  }
+            desc = "4th floating toggleterm",
+          },
+        },
+      },
+    },
+  },
 }
