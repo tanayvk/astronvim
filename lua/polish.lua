6e793ec48b7b00b9a0d0fde75d7d0b4dfe73bdfd
Tanay Karnik <tanayvk@gmail.com>
2024-11-15 18:41:29 +0530

    fix clipboard

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,4 +2,18 @@
 -- things like custom filetypes. This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
-vim.g.clipboard = ""
+-- vim.g.clipboard = {
+--   name = "WslClipboard",
+--   copy = {
+--     ["+"] = "clip.exe",
+--   },
+--   paste = {
+--     ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--   },
+--   cache_enabled = 0,
+-- }
+
+-- Clear the clipboard option whenever entering a buffer
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function() vim.opt.clipboard = "" end,
+})
